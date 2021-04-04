a76b3ebf290c4c40941b918d3f53565152c5a568
Darius Wattimena <dariuswattimena@hotmail.com>
2021-04-04 15:00:09 +0200

    Added support for slider velocity on the trigger distance calculator

--- src/components/tools/hyperTriggerDistance/HyperTriggerDistanceCalculator.tsx
+++ src/components/tools/hyperTriggerDistance/HyperTriggerDistanceCalculator.tsx
@@ -3,13 +3,14 @@ import HyperTriggerDistanceForm from './HyperTriggerDistanceForm'
 
 export default function HyperTriggerDistanceCalculator() {
   const [circleSize, setCircleSize] = useState(4.0)
-  const [bpm, setBpm] = useState(180)
+  const [bpm, setBpm] = useState(180.0)
+  const [sliderVelocity, setSliderVelocity] = useState(1.0)
   const [result, setResult] = useState<SnappingResult[]>([])
 
   useEffect(() => {
     const snaps = calculateSnaps()
     setResult(calculateTriggerDistances(snaps))
-  }, [circleSize, bpm])
+  }, [circleSize, bpm, sliderVelocity])
 
   function calculateSnaps(): MillisecondSnappingReference {
     const oneMeasureMs = 60000 / bpm
@@ -26,7 +27,7 @@ export default function HyperTriggerDistanceCalculator() {
 
   function getTriggerDistance(ms: number, dashRange: number): number {
     const timeToNext = ms - 1000 / 60 / 4
-    return Math.ceil(timeToNext + dashRange)
+    return timeToNext + dashRange
   }
 
   function calculateTriggerDistances(snaps: MillisecondSnappingReference): SnappingResult[] {
@@ -34,37 +35,49 @@ export default function HyperTriggerDistanceCalculator() {
     const fruitWidth = (64.0 * (1.0 - 0.699999988079071 * catchDifficulty)) / 128.0
     const catcherWidth = 305.0 * fruitWidth * 0.7
     const dashRange = catcherWidth / 2.0
+    const whiteTickDistance = getTriggerDistance(snaps.whiteTick, dashRange)
+    const redTickDistance = getTriggerDistance(snaps.redTick, dashRange)
+    const purpleTickDistance = getTriggerDistance(snaps.purpleTick, dashRange)
+    const blueTickDistance = getTriggerDistance(snaps.blueTick, dashRange)
+    const darkPurpleTickDistance = getTriggerDistance(snaps.darkPurpleTick, dashRange)
+    const yellowTickTickDistance = getTriggerDistance(snaps.yellowTick, dashRange)
 
     return [
       {
         snapping: Snapping.WHITE_TICK,
         msSnapping: snaps.whiteTick,
-        distanceNeed: getTriggerDistance(snaps.whiteTick, dashRange)
+        xDistance: whiteTickDistance / (sliderVelocity * 100),
+        distanceNeed: whiteTickDistance
       },
       {
         snapping: Snapping.RED_TICK,
         msSnapping: snaps.redTick,
-        distanceNeed: getTriggerDistance(snaps.redTick, dashRange)
+        xDistance: redTickDistance / (sliderVelocity * 100) * 2,
+        distanceNeed: redTickDistance
       },
       {
         snapping: Snapping.PURPLE_TICK,
         msSnapping: snaps.purpleTick,
-        distanceNeed: getTriggerDistance(snaps.purpleTick, dashRange)
+        xDistance: purpleTickDistance / (sliderVelocity * 100) * 3,
+        distanceNeed: purpleTickDistance
       },
       {
         snapping: Snapping.BLUE_TICK,
         msSnapping: snaps.blueTick,
-        distanceNeed: getTriggerDistance(snaps.blueTick, dashRange)
+        xDistance: blueTickDistance / (sliderVelocity * 100) * 4,
+        distanceNeed: blueTickDistance
       },
       {
         snapping: Snapping.DARK_PURPLE_TICK,
         msSnapping: snaps.darkPurpleTick,
-        distanceNeed: getTriggerDistance(snaps.darkPurpleTick, dashRange)
+        xDistance: darkPurpleTickDistance / (sliderVelocity * 100) * 6,
+        distanceNeed: darkPurpleTickDistance
       },
       {
         snapping: Snapping.YELLOW_TICK,
         msSnapping: snaps.yellowTick,
-        distanceNeed: getTriggerDistance(snaps.yellowTick, dashRange)
+        xDistance: yellowTickTickDistance / (sliderVelocity * 100) * 8,
+        distanceNeed: yellowTickTickDistance
       }
     ]
   }
@@ -77,6 +90,8 @@ export default function HyperTriggerDistanceCalculator() {
           setBpm={setBpm}
           circleSize={circleSize}
           setCircleSize={setCircleSize}
+          sliderVelocity={sliderVelocity}
+          setSliderVelocity={setSliderVelocity}
         />
       </div>
       <div className="col-6">
@@ -84,9 +99,13 @@ export default function HyperTriggerDistanceCalculator() {
           <table className="col-12">
             <thead>
               <tr>
-                <th>Snap</th>
-                <th>Milliseconds (rounded)</th>
-                <th>Trigger distance (in osu pixels)</th>
+                <th rowSpan={2}>Snap</th>
+                <th rowSpan={2}>Milliseconds</th>
+                <th colSpan={2}>Trigger distance</th>
+              </tr>
+              <tr>
+                <th>Osu Pixels</th>
+                <th>X Value</th>
               </tr>
             </thead>
             <tbody>
@@ -96,7 +115,8 @@ export default function HyperTriggerDistanceCalculator() {
                   <tr key={key}>
                     <td>{value.snapping}</td>
                     <td>{Math.round(value.msSnapping)}</td>
-                    <td>{value.distanceNeed}</td>
+                    <td>{Math.ceil(value.distanceNeed)}</td>
+                    <td>{Math.ceil(value.xDistance * 100) / 100}</td>
                   </tr>
                 )
               })}
@@ -120,6 +140,7 @@ interface MillisecondSnappingReference {
 interface SnappingResult {
   snapping: Snapping
   msSnapping: number
+  xDistance: number
   distanceNeed: number
 }
 
--- src/components/tools/hyperTriggerDistance/HyperTriggerDistanceForm.tsx
+++ src/components/tools/hyperTriggerDistance/HyperTriggerDistanceForm.tsx
@@ -6,18 +6,22 @@ interface HyperTriggerDistanceFormProps {
   setBpm: React.Dispatch<React.SetStateAction<number>>
   circleSize: number
   setCircleSize: React.Dispatch<React.SetStateAction<number>>
+  sliderVelocity: number
+  setSliderVelocity: React.Dispatch<React.SetStateAction<number>>
 }
 
 export default function HyperTriggerDistanceForm({
   bpm,
   setBpm,
   circleSize,
-  setCircleSize
+  setCircleSize,
+  sliderVelocity,
+  setSliderVelocity
 }: HyperTriggerDistanceFormProps) {
   return (
     <form className="row">
       <div className="col-12 note-col">
-        <p className="note">NOTE: decimal numbers work for both BPM and Circle Size (e.g. 4,5)</p>
+        <p className="note">NOTE: decimal numbers work for all fields (e.g. 4,5)</p>
       </div>
       <label htmlFor="bpm" className="col-12">
         BPM
@@ -29,15 +33,26 @@ export default function HyperTriggerDistanceForm({
           min={1}
         />
       </label>
-      <label htmlFor="bpm" className="col-12">
+      <label htmlFor="cs" className="col-12">
         Circle Size
         <input
-          id="bpm"
+          id="cs"
           type="number"
           value={circleSize}
           onChange={event => setCircleSize(Number(event.target.value))}
           min={0}
           max={10}
+        />
+      </label>
+      <label htmlFor="sv" className="col-12">
+        Slider Velocity
+        <input
+          id="sv"
+          type="number"
+          value={sliderVelocity}
+          onChange={event => setSliderVelocity(Number(event.target.value))}
+          min={0.4}
+          max={3.6}
         />
       </label>
     </form>
