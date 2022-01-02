7f426d085d4dfc4995548a61949347a94b52856d
Darius Wattimena <dariuswattimena@hotmail.com>
2022-01-03 00:19:59 +0100

    Fixed the spacing of the notes on the HyperTriggerDistanceForm

--- src/components/tools/hyperTriggerDistance/HyperTriggerDistanceForm.tsx
+++ src/components/tools/hyperTriggerDistance/HyperTriggerDistanceForm.tsx
@@ -2,28 +2,26 @@ import React from 'react'
 
 export default function HyperTriggerDistanceForm() {
   return (
-    <form className="row">
-      <div className="col-12 note-col">
-        <p className="note">
-          <ul>
-            <li>
-              <strong className="fake-bold">Decimal numbers work for all fields.</strong> For
-              example 4.5 for circle size.
-            </li>
-            <li>
-              <strong className="fake-bold">Osu Pixels can be 1 pixel off.</strong> The current
-              version of osu stable seems to cast to an integer with hyperdash generation but
-              calculations here are done using ms values, not based of snapping like is done in the
-              editor.
-            </li>
-            <li>
-              <strong className="fake-bold">X Distance is an estimate.</strong> The current version
-              of osu stable does some strange roundings, the purpose of this value is to give you
-              somewhat of an idea what value to use for your hyperdashes.
-            </li>
-          </ul>
-        </p>
-      </div>
-    </form>
+    <div className="note-col">
+      <p className="note">
+        <ul>
+          <li>
+            <strong className="fake-bold">Decimal numbers work for all fields.</strong> For
+            example 4.5 for circle size.
+          </li>
+          <li>
+            <strong className="fake-bold">Osu Pixels can be 1 pixel off.</strong> The current
+            version of osu stable seems to cast to an integer with hyperdash generation but
+            calculations here are done using ms values, not based of snapping like is done in the
+            editor.
+          </li>
+          <li>
+            <strong className="fake-bold">X Distance is an estimate.</strong> The current version
+            of osu stable does some strange roundings, the purpose of this value is to give you
+            somewhat of an idea what value to use for your hyperdashes.
+          </li>
+        </ul>
+      </p>
+    </div>
   )
 }
