a9c75f44f4211005c8580d79c12ab6b2f9df33d5
Darius Wattimena <dariuswattimena@hotmail.com>
2022-01-03 00:21:22 +0100

    Code formatting

--- src/components/tools/hyperTriggerDistance/HyperTriggerDistanceCalculator.tsx
+++ src/components/tools/hyperTriggerDistance/HyperTriggerDistanceCalculator.tsx
@@ -109,13 +109,13 @@ export default function HyperTriggerDistanceCalculator({
               <li>
                 <strong className="fake-bold">Osu Pixels can be 1 pixel off.</strong> The current
                 version of osu stable seems to cast to an integer with hyperdash generation but
-                calculations here are done using ms values, not based of snapping like is done in the
-                editor.
+                calculations here are done using ms values, not based of snapping like is done in
+                the editor.
               </li>
               <li>
-                <strong className="fake-bold">X Distance is an estimate.</strong> The current version
-                of osu stable does some strange roundings, the purpose of this value is to give you
-                somewhat of an idea what value to use for your hyperdashes.
+                <strong className="fake-bold">X Distance is an estimate.</strong> The current
+                version of osu stable does some strange roundings, the purpose of this value is to
+                give you somewhat of an idea what value to use for your hyperdashes.
               </li>
             </ul>
           </p>
